bee887c0fcfcbda23b50ee34500653e844146261
Dominik Čustović <domcus44@gmail.com>
2020-05-06 15:37:27 +0200

    setting New Article option

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const server = express();
+const articleRouter = require('./routes/articles');
+
 
 // i'm going to write all my views using ejs
 // view engine is going to covert ejs code to html code
 server.set('view engine', 'ejs');  
 
+server.use('/articles', articleRouter);
 
 server.get('/', (req, res) => {
-    res.send('testing')
+    const articles = [{
+        title: "Test title",
+        createdAt: new Date(),
+        description: "Test description"
+    },
+    {
+        title: "Test title 2",
+        createdAt: new Date(),
+        description: "Test description 2"
+    }];
+    res.render('articles/index', { articles : articles });
 });
 
 
