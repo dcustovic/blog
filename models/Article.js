bfb1afc8e3d211d1d02562f634931801668eed62
Dominik Čustović <domcus44@gmail.com>
2020-05-07 21:37:07 +0200

    sanitizeing markdown

--- models/Article.js
+++ models/Article.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database')
 const marked = require('marked');
+const createDomPurify = require('dompurify');
+// it's in brackets because i want only that jsdom portion of what it returns
+const { JSDOM } = require('jsdom');
+// allows dompurify to create HTML and purify it using JSDOM().window object
+const dompurify = createDomPurify(new JSDOM().window);
 
 const Article = db.define('article', {
     title: {
@@ -21,8 +26,27 @@ const Article = db.define('article', {
     updatedAt: {
         type: Date,
         default: Date.now
+    },
+    sanitizedHtml: {
+        type: Sequelize.STRING,
+        require: true
     }
 });
+
+Article.beforeValidate((article, options) => {
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    };
+  });
+  
+
+
+
+
+
+    // converts markdown to HTML and purify that HTML to get rid of any malicious code
+    // and to escape all HTML characters
+
 
 
 module.exports = Article;
