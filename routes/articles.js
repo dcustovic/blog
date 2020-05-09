68a527ed0409447bc026f569a81a0ecee7875cc3
Dominik Čustović <domcus44@gmail.com>
2020-05-09 14:15:03 +0200

    setting Edit

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
 const Article = require('../models/Article');
 
 
@@ -14,19 +13,22 @@ router.get('/:id', async (req, res) => {
     res.render('articles/show', { article: article })
 });
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-    });
-    try {
-        article = await article.save();
-        res.redirect(`/articles/${article.id}`);
-    } catch (e) {
-        console.log(e)
-        res.render('articles/new', { article: article });
-    }
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findByPk(req.params.id)
+    res.render('articles/edit', { article: article })
 });
+
+
+router.post('/', async (req, res, next) => {
+    req.article = new Article();
+    next();
+}, saveArticleAndRedirect('new'))
+
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findByPk(req.params.id)
+    next();
+}, saveArticleAndRedirect('edit'));
 
 router.delete('/:id', async (req, res) => {
     await Article.findByPk(req.params.id).then((article) => {
@@ -35,5 +37,22 @@ router.delete('/:id', async (req, res) => {
         res.redirect('/')
     });
 });
+
+
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        try {
+            article = await article.save();
+            res.redirect(`/articles/${article.id}`);
+        } catch (e) {
+            console.log(e)
+            res.render(`articles/${path}`, { article: article });
+        };
+    };
+};
 
 module.exports = router
